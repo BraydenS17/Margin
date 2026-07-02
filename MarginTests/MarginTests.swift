f6d98014ec00676585880a3a8552ae28aa139ac4
BraydenS17 <braydensally@gmail.com>
2026-07-02 19:25:27 -0400

    Initial Commit

---
+++ MarginTests/MarginTests.swift
@@ -0,0 +1,18 @@
+//
+//  MarginTests.swift
+//  MarginTests
+//
+//  Created by Brayden Sally on 2026-07-02.
+//
+
+import Testing
+
+struct MarginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
